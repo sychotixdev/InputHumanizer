ecabee56fe3fc4b74689249cc6b97df37751e860
instant.sc <code@insc.cc>
2023-11-30 00:27:30 +0100

    Clean up

--- Input/Mouse.cs
+++ Input/Mouse.cs
@@ -2,25 +2,14 @@
 using Kalon;
 using Kalon.Native.Structs;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
-using System.Runtime.InteropServices;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using Vanara.PInvoke;
 
 namespace InputHumanizer.Input
 {
     internal class Mouse
     {
-        public static POINT GetCursorPosition()
-        {
-            User32.GetCursorPos(out POINT lpPoint);
-
-            return lpPoint;
-        }
-
         private static float NormalizeDistance(float distance, float maxDistance)
         {
             return Math.Min(distance / maxDistance, 1.0f);
@@ -31,11 +20,11 @@ namespace InputHumanizer.Input
             return (1 - interpolationFactor) * startValue + interpolationFactor * endValue;
         }
 
-        public static async SyncTask<bool> MoveMouse(Vector2 targetPosition, int maxInterpolationDistance = 700, int minInterpolationDelay = 0, int maxInterpolationDelay = 300)
+        public static async SyncTask<bool> MoveMouse(Vector2 targetPosition, int maxInterpolationDistance = 700, int minInterpolationDelay = 0, int maxInterpolationDelay = 300, CancellationToken cancellationToken = default)
         {
-            POINT currentPosition = GetCursorPosition();
+            var currentPosition = ExileCore.Input.ForceMousePositionNum;
 
-            float distance = Vector2.Distance(new Vector2(currentPosition.X, currentPosition.Y), targetPosition);
+            float distance = Vector2.Distance(currentPosition, targetPosition);
             float normalizedDistance = NormalizeDistance(distance, maxInterpolationDistance);
 
             float interpolatedValue = Lerp(minInterpolationDelay, maxInterpolationDelay, normalizedDistance);
@@ -44,20 +33,18 @@ namespace InputHumanizer.Input
 
             var movements = CursorMover.GenerateMovements(new Point(currentPosition.X, currentPosition.Y), new Point((int)targetPosition.X, (int)targetPosition.Y), (int)mouseSpeed.TotalMilliseconds);
 
-            foreach( var movement in movements )
+            foreach( var movement in movements)
             {
                 // First, we need to loop through and spam SetCursorPos to get us to each location
                 foreach(var point in movement.Points)
                 {
-                    User32.SetCursorPos(point.X, point.Y);
+                    ExileCore.Input.SetCursorPos(new Vector2(point.X, point.Y));
                 }
 
-                await Task.Delay(movement.Delay);
+                await Task.Delay(movement.Delay, cancellationToken);
             }
 
             return true;
         }
-
-
     }
 }
